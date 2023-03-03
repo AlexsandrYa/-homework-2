e06713fe954403cd6aea38512e817e1675741f03
Yavtushenko Aleksandr <yavtushenko.aa@yandex.ru>
2023-03-03 03:11:52 +0300

    added the code

--- src/test/java/CheckingForCompletion.java
+++ src/test/java/CheckingForCompletion.java
@@ -1,17 +1,33 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CheckingForCompletion {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
 
 
 @Test
     void fillFromTest(){
 
-    open ("https://demoqa.com/automation-practice-form");
-    $("").setValue("");
-    $("").setValue("");
+
+
+    open ("/text-box");
+    $("#userName").setValue("Sasha");
+    $("#userEmail").setValue("yaya@yandex.ru");
+    $("#currentAddress").setValue("Krasnodar");
+    $("#permanentAddress").setValue("Saratov");
+    $("#submit").click();
+
+    $("#output").shouldHave(text("Sasha"), text("yaya@yandex.ru"), text("Krasnodar"),
+            text("Saratov"));
 }
 
 }
